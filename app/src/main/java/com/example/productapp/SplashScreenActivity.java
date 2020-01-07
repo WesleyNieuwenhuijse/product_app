5091f86bdc0d9b2a6116fc20172d1c2931bba5d2
wesley <wn.sientje@gmail.com>
2020-01-07 16:13:14 +0100

    initial

---
+++ app/src/main/java/com/example/productapp/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package com.example.productapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(2500)
+                .withBackgroundColor(Color.parseColor("#124356"))
+                .withFooterText("ProductApp")
+                .withLogo(R.mipmap.ic_launcher_round)
+                .withAfterLogoText("find your favourite product and their properties")
+                ;
+        config.getFooterTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+
+        View easySplashScreen = config.create();
+        setContentView(easySplashScreen);
+    }
+}
