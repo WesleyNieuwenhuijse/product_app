154128c3f416bcb0780d644f69eafe83055700f0
Carmen Luna <carmen.oprea@hotmail.com>
2020-01-21 19:31:49 +0100

    shop list and shop details activities created

--- app/src/main/java/com/example/productapp/MainActivity.java
+++ app/src/main/java/com/example/productapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.productapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void viewShops(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, ShopListListActivity.class);
+        startActivity(intent);
     }
 }
